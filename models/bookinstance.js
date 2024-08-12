e16b6c5bdfb7607cf10e14aa56cb20b2f611a474
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-08-12 17:08:41 +0100

    added the virtual for the due date format

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -25,7 +25,7 @@ const BookInstanceSchema = new Schema({
 })
 
 BookInstanceSchema.virtual("url").get(function () {
-    return `/catalog/bookinstance${this._id}`
+    return `/catalog/bookinstance/${this._id}`
 })
 BookInstanceSchema.virtual('due_back_formatted').get(function () {
     return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED)
