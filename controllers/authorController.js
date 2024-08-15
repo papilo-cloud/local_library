7dd9fbb3fbafab4affbdc997e306663e25828d4d
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-08-15 16:49:31 +0100

    added the control for deleting author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -86,13 +86,49 @@ exports.author_create_post = [
             res.redirect(author.url)
         }
 })]
+
 // Display Author delete form on GET
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Author delete GET`)
+
+    // Get details of all authors and their books in parallel.
+    const [author, allBooksByAuthor] = await Promise.all([
+        Author.findById(req.params.id).exec(),
+        Book.find({author: req.params.id}, 'title summary').exec()
+    ])
+
+    if (author === null) {
+        // NO results.
+        res.redirect('/catalog/authors')
+    }
+    res.render('author_delete', {
+        title: 'Delete Author',
+        author,
+        author_books: allBooksByAuthor
+    })
 })
+
 // Handle Author delete on POST
 exports.author_delete_post = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Author delete POST`)
+
+    // Get details of all authors and their books in parallel.
+    const [author, allBooksByAuthor] = await Promise.all([
+        Author.findById(req.params.id).exec(),
+        Book.find({author: req.params.id}, 'title summary').exec()
+    ])
+
+    if (allBooksByAuthor.length > 0) {
+        // Author has books.
+        res.render('author_delete', {
+            title: 'Delete Author',
+            author,
+            author_books: allBooksByAuthor
+        })
+        return
+    } else {
+        // Author has no book. Delete object and redirect to the list of authors.
+        await Author.findByIdAndDelete(req.body.authorid)
+        res.redirect('/catalog/authors')
+    }
 })
 // Display Author update form on GET
 exports.author_update_get = asyncHandler(async (req, res, next) => {
